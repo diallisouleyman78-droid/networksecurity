85ff1780e7e427ba1643bd8eb13d320508019a63
Diallo Souleyman <diallisouleyman78@gmail.com>
2026-03-24 00:08:36 +0100

    batch prediction

--- app.py
+++ app.py
@@ -2,7 +2,9 @@ import sys, os
 
 import certifi
 
-from networksecurity.constants.training_pipeline import DATA_INGESTION_COLLECTION_NAME, DATA_INGESTION_DATABASE_NAME 
+from networksecurity.constants.training_pipeline import DATA_INGESTION_COLLECTION_NAME, DATA_INGESTION_DATABASE_NAME
+from networksecurity.utils.ml_utils.model.estimator import NetworkModel
+from networksecurity.utils.ml_utils.model.estimator import NetworkModel 
 ca = certifi.where() # use the certifi package to get the path to the certificate authority bundle which is used to verify the SSL certificates when making requests to external APIs. This is important because we want to ensure that our application is secure and that we are not making requests to malicious APIs that could potentially steal our data or compromise our application. By using the certifi package, we can ensure that we are only making requests to trusted APIs that have valid SSL certificates.
 
 from dotenv import load_dotenv
@@ -23,6 +25,9 @@ from starlette.responses import RedirectResponse
 import pandas as pd
 
 from networksecurity.utils.main_utils.utils import load_object
+
+from fastapi.templating import Jinja2Templates
+templates = Jinja2Templates(directory="./templates")
 
 
 client = pymongo.MongoClient(mongo_db_url, tlsCAFile=ca)
@@ -55,5 +60,28 @@ async def train_route():
     except Exception as e:
         raise NetworkSecurityException(e, sys) from e
     
+
+@app.post("/predict")    
+async def predict_route(request:Request, file: UploadFile = File(...)):
+    try:
+        df = pd.read_csv(file.file)
+        preprocessor = load_object("final_model/preprocessor.pkl")
+        final_model = load_object("final_model/model.pkl")
+        network_model = NetworkModel(preprocessor=preprocessor, model=final_model)
+        print(df.iloc[0])
+        y_pred = network_model.predict(df)
+        print(y_pred)
+        df['predicted_column'] = y_pred
+
+        df.to_csv("prediction_output/output.csv")
+        table_html = df.to_html(classes='table table-striped')
+        return templates.TemplateResponse("tables.html", {"request": request, "table_html": table_html})
+        
+    except Exception as e:
+        raise NetworkSecurityException(e, sys) from e
+
+
+
+
 if __name__ == "__main__":
     app_run(app, host="0.0.0.0", port=8000)    
